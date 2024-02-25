dcf98c2168af66a10688b3c13bb591cdfbe033e5
Francis Uyaemesi <anthoniofrank24@gmail.com>
2024-02-25 14:31:09 -0800

    puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,23 @@
-# Using Puppet, install flask from pip3
+# Puppet mainfest to install Flask version 2.1.0 using pip3
+
+package { 'python3-pip':
+  ensure   => installed,
+}
+
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => '/usr/local/bin:/usr/bin:/bin',
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+  require => Package['python3-pip'],
 }
